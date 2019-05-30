1ed09830ba084acc046b86643a4cc0e1a634e38e
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-05-30 15:48:59 +0800

    update gitignore file

--- knexfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './data/lambda.db3', // the folder will be created when we run the migrations
-    },
-  },
-};
